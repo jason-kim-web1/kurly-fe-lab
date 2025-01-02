55af9bd480a082c3a3d868d347e5db56fb4639dd
jason-kim-web1 <jason.kim@kurlycorp.com>
2025-01-02 16:57:13 +0900

    refactor: 쉘 스크립트 파일 줄바꿈 적용

--- kurly-rproxy/certgen.sh
+++ kurly-rproxy/certgen.sh
@@ -2,4 +2,11 @@
 rm -rf certs
 mkdir -p certs && cd $_
 mkcert -install
-mkcert -key-file key.pem -cert-file cert.pem www.local.kurly.com www.local.dev.kurly.com www.local.stg.kurly.com www.local.perf.kurly.com localhost 127.0.0.1 ::1
+mkcert -key-file key.pem -cert-file cert.pem \
+www.local.kurly.com \
+www.local.dev.kurly.com \
+www.local.stg.kurly.com \
+www.local.perf.kurly.com \
+localhost \
+127.0.0.1 \
+::1
